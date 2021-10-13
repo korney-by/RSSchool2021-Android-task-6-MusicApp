8b534c4fe7c7871c9f22d6e67667e70e70e13068
Sergey Kornienko <korney.by@gmail.com>
2021-10-14 01:53:00 +0300

    Initial commit

---
+++ app/src/main/java/com/korneysoft/rsschool2021_android_task_6_musicapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.korneysoft.rsschool2021_android_task_6_musicapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
