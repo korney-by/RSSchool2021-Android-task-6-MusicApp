f9c6cfa7c89f220da48d85ecf612bc0550ff1bf6
korney <korney.by@gmail.com>
2021-10-15 15:06:02 +0300

    study: dealing with dagger

--- app/src/main/java/com/korneysoft/rsschool2021_android_task_6_musicapp/di/AppComponent.kt
+++ app/src/main/java/com/korneysoft/rsschool2021_android_task_6_musicapp/di/AppComponent.kt
@@ -1,18 +1,17 @@
 package com.korneysoft.rsschool2021_android_task_6_musicapp.di
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.korneysoft.rsschool2021_android_task_6_musicapp.MainActivity
+import com.korneysoft.rsschool2021_android_task_6_musicapp.R
+import com.korneysoft.rsschool2021_android_task_6_musicapp.data.Track
+import com.korneysoft.rsschool2021_android_task_6_musicapp.data.Tracks
 import dagger.Component
 import dagger.Module
 import dagger.Provides
-import android.app.Application
-import com.korneysoft.rsschool2021_android_task_6_musicapp.data.Tracks
-
-import javax.inject.Singleton
 
 
-
-
-@Component(modules = [AppModule::class])
+@Component(modules = [DataModule::class])
 interface AppComponent {
     fun inject(activity: MainActivity)
     val tracks: Tracks
@@ -22,8 +21,18 @@ interface AppComponent {
 object DataModule {
 
     @Provides
-    fun provideTracks(
+    fun provideTracks(): List<Track> {
+        val gson = Gson()
+        val jsonFile: String = ApplicationContext.assets.open("ela.json").bufferedReader().use {it.readText()}
+        return gson.fromJson(jsonFile, Tracks.ListOfTrack::class.java)
 
-    )
+    }
+
+    @Provides
+    fun providerGson(): Gson? {
+        val gsonBuilder = GsonBuilder()
+        return gsonBuilder.create()
+    }
+
 
 }
